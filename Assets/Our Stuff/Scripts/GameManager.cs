38afe274c3dff53ea1df65fa9fadafa710aedfbe
RonTheDragon <gweta.ron@gmail.com>
2022-11-20 21:50:45 +0200

    Code is now pretty

--- Assets/Our Stuff/Scripts/GameManager.cs
+++ Assets/Our Stuff/Scripts/GameManager.cs
@@ -1,37 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [HideInInspector] public static GameManager instance;
-    public int PlayersAmount = 0;
-    bool TurnedOffStartingCam;
+    // Visible //
+
+    [Tooltip("List of all The Players")]
+    [ReadOnly][SerializeField] GameObject[] Players = new GameObject[4];
+
+    [Tooltip("True - will Remove a player that disconnected his Controller\n\nFalse - will Keep the player inside, allowing him to rejoin to where he left off")]
+    public bool LeaveOnDisconnect;
+
+    [Header("Camera Layers For Each Player")]
+    [Tooltip("These LayerMasks are needed for local Co-op so Cinemachine can work properly\n\n Just Make Sure Each Layer Mask cant see other numbered player but itself")]
     public LayerMask[] PlayerMasks = new LayerMask[4];
 
+    // Invisible //
+
+    [HideInInspector] public static GameManager instance;
+    bool _turnedOffStartingCam;
+    GameObject _noPlayersCamera => transform.GetChild(0).gameObject;
+
+    // Starts Before The Game Starts
     private void Awake()
     {
-        GameManager.instance = this;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary> When Any Player Joins </summary>
+    public void PlayerJoined()
     {
-        
-    }
-
-    public void AddPlayer()
-    {
-        PlayersAmount++;
-        if (!TurnedOffStartingCam)
+        if (!_turnedOffStartingCam)
         {
-            transform.GetChild(0).gameObject.SetActive(false);
-            TurnedOffStartingCam = true;
+            _noPlayersCamera.SetActive(false);
+            _turnedOffStartingCam = true;
         }
     }
+
+    /// <summary> When "LeaveOnDisconnect" is true and someone left </summary>
+    public void PlayerLeft(int n)
+    {
+            Players[n - 1] = null;
+            // Turn On No Players Camera if there are no players left        
+            if (0 == CountPlayers())
+            {
+                _noPlayersCamera.SetActive(true);
+                _turnedOffStartingCam = false;
+            }
+    }
+
+    /// <summary> Players Call this Function When they join to update the Game Manager </summary>
+    public int AddPlayer(GameObject Player)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Players[i] == null)
+            {
+                Players[i] = Player;
+                return (i+1);
+            }
+        } 
+        return 0;
+    }
+
+    /// <summary> Returns The Amount of Players Currently Playing </summary>
+    public int CountPlayers()
+    {
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (Players[i] != null)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
+
+#region Magic Trick That enables ReadOnly
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,
+                                            GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position,
+                               SerializedProperty property,
+                               GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endregion
--- Assets/Our Stuff/Scripts/ThirdPersonMovement.cs
+++ Assets/Our Stuff/Scripts/ThirdPersonMovement.cs
@@ -1,136 +1,102 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq.Expressions;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//using UnityEngine.UIElements;
-
 public class ThirdPersonMovement : MonoBehaviour
 {
-    [Header("Walking")]
-    [SerializeField] float Speed = 10;
-    [Header("Jumping")]
-    [SerializeField] float Gravity = 10;
-    CharacterController CC => GetComponent<CharacterController>();
-    Camera MyCam => cam.GetComponent<Camera>();
-    [SerializeField] Transform cam;
-    [SerializeField] CinemachineFreeLook cfl;
-    InputHandler inputHandler => cfl.GetComponent<InputHandler>();
-    PlayerInput playerInput => GetComponent<PlayerInput>();
+    // Visible //
 
+    [ReadOnly][SerializeField] int PlayerNumber;
+
+    [Header("Walking")]
+    [Tooltip("The movement speed of the player")]
+    [SerializeField] float Speed = 10;
+
+    [Header("Jumping")]
+    [Tooltip("The Height of the jumps")]
+    [SerializeField] float Jump = 20;
+    [Tooltip("The Falling Speed")]
+    [SerializeField] float Gravity = 10;
+
+    [Header("Sliding")]
+    [Tooltip("on What floor angle we start to slide and cant jump on")]
+    [SerializeField] float slopeLimit = 45;
+    [Tooltip("The Speed of sliding")]
+    [SerializeField] float SlideSpeed = 5;
+    [Tooltip("Are we sliding?")]
+    [ReadOnly][SerializeField] bool isSliding;
+    [Tooltip("The Normal of the floor, (how steep is the floor)")]
+    [ReadOnly][SerializeField] Vector3 hitNormal;
+
+    [Header("Ground Check")]
+    [Tooltip("the Y position of the Ground Checkbox")]
+    [SerializeField] float Y;
+    [Tooltip("how wide the Ground Checkbox")]
+    [SerializeField] float Wide = 0;
+    [Tooltip("how tall the Ground Checkbox")]
+    [SerializeField] float Height = .15f;
+    [Tooltip("a layer that contains anything we can jump on")]
+    [SerializeField] LayerMask Jumpable;
+    [Tooltip("Are we On The Ground?")]
+    [ReadOnly][SerializeField] bool isGrounded;
+
+    [Header("References")]
+    [Tooltip("Place The Player's Camera Here")]
+    [SerializeField] Transform cam;
+    [Tooltip("Place The Player's Cinemachine Here")]
+    [SerializeField] CinemachineFreeLook cfl;
+
+    // Invisible //
+
+    // Auto Referencing
+    CharacterController CC => GetComponent<CharacterController>();
+    Camera _cam => cam.GetComponent<Camera>();
+    InputHandler _inputHandler => cfl.GetComponent<InputHandler>();
+    PlayerInput _playerInput => GetComponent<PlayerInput>();
+    GameManager GM => GameManager.instance;
+
+    // Stored Data
+    Vector2 _movement;
+    bool _jumped;
+    float _gravityPull;
+    Vector3 _forceDirection;
+    float   _forceStrength;
     float f;
 
-    Vector3 ForceDirection;
-    float   ForceStrength;
+    // Ground Check 
+    Vector3 _boxPosition => CC.transform.position + (Vector3.up * CC.bounds.extents.y) * Y;
+    Vector3 _boxSize => new Vector3(CC.bounds.extents.x + Wide, Height * 2, CC.bounds.extents.z + Wide);
 
-    [SerializeField] float Jump = 20;
-    [SerializeField] float gravityPull;
-
-    Vector3 hitNormal;
-
-    [SerializeField] float slopeLimit = 80;
-
-    [SerializeField] float slideFriction;
-
-    Vector2 movement;
-    Vector2 look;
-    bool Jumped;
-
-    public int PlayerNumber;
-
-    public float Y;
-    public float Wide = 0;
-    public float Height = .15f;
-    public LayerMask Jumpable;
-    Vector3 BoxOrigin => CC.transform.position + (Vector3.up * CC.bounds.extents.y) * Y;
-    Vector3 RayCastSize => new Vector3(CC.bounds.extents.x + Wide, Height * 2, CC.bounds.extents.z + Wide);
-
-    bool isGrounded;
-    public bool isSliding;
-    GameManager GM => GameManager.instance;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible   = false;
-        SetUpPlayer();
+        setUpPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GroundCheck();
-        Jumping();
-        Movement();
-        AddForce();
+        groundCheck();
+        gravitation();
+        jumping();
+        movement();
+        applyingForce();
         slide();
     }
 
-    private void GroundCheck()
+    /// <summary> Allows the player to walk. </summary>
+    void movement()
     {
-        isGrounded = Physics.CheckBox(BoxOrigin, RayCastSize, quaternion.identity, Jumpable);
-        if (isGrounded)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(BoxOrigin + Vector3.up * 0.5f, Vector3.down, out hit, 1, Jumpable))
-            {
-                hitNormal = hit.normal;
-                //Debug.Log(hit.transform.name);
-            }
-        }
-        else
-        {
-            hitNormal = hitNormal * 0.99f;
-        }
-        isSliding = (!(Vector3.Angle(Vector3.up, hitNormal) <= slopeLimit));
-    }
-
-    private void AddForce()
-    {
-        if (ForceStrength > 0)
-        {
-            CC.Move(ForceDirection.normalized * ForceStrength * Time.deltaTime);
-            ForceStrength -= ForceStrength * 2 * Time.deltaTime;
-        }
-    }
-
-    private void Jumping()
-    {
-        if (isGrounded)
-        {
-            gravityPull = 0.1f;
-        }
-        else if (gravityPull < 1)
-        {
-            gravityPull += 0.2f * Time.deltaTime;
-        }
-        CC.Move(Vector3.down * Gravity * gravityPull * Time.deltaTime);
-
-        if (Jumped && isGrounded && !isSliding)
-        {
-            AddForce(Vector3.up, Jump);
-        }
-    }
-
-    private void slide()
-    {
-        if (isSliding)
-        {
-            Vector3 slid = Vector3.zero;
-            slid.x += ((1f - hitNormal.y) * hitNormal.x * (1f - slideFriction));
-            slid.z += ((1f - hitNormal.y) * hitNormal.z * (1f - slideFriction));
-
-            CC.Move(slid);
-        }
-    }
-
-    void Movement()
-    {
-        Vector2 Movement = movement.normalized; //Get input from player for movem
+        Vector2 Movement = _movement.normalized; //Get input from player for movem
 
         float targetAngle = Mathf.Atan2(Movement.x, Movement.y) * Mathf.Rad2Deg + cam.eulerAngles.y; //get where player is looking
         float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref f, 0.1f); //Smoothing
@@ -143,51 +109,127 @@ public class ThirdPersonMovement : MonoBehaviour
         }
     }
 
-    void AddForce(Vector3 dir, float force)
+    /// <summary> Allows the player to jump. </summary>
+    private void jumping()
     {
-        ForceDirection = dir;
-        ForceStrength  = force;
+        if (_jumped && isGrounded && !isSliding)
+        {
+            AddForce(Vector3.up, Jump);
+        }
     }
 
-    void SetUpPlayer()
+    /// <summary> Takes care of Gravity </summary>
+    private void gravitation()
     {
-        inputHandler.horizontal = playerInput.actions.FindAction("Look"); // Set up Looking Controls 
-
-        PlayerNumber = GameManager.instance.PlayersAmount; // Give The Player a Number
-        switch (PlayerNumber)
+        if (isGrounded)
         {
-            case 1: cfl.gameObject.layer = LayerMask.NameToLayer("Player1"); MyCam.cullingMask = GM.PlayerMasks[0];
+            _gravityPull = .1f;
+        }
+        else if (_gravityPull < 1)
+        {
+            _gravityPull += .2f * Time.deltaTime;
+        }
+        CC.Move(Vector3.down * Gravity * _gravityPull * Time.deltaTime);
+    }
+
+    /// <summary> Checking the ground and tell the player if he is grounded or sliding. </summary>
+    private void groundCheck()
+    {
+        isGrounded = Physics.CheckBox(_boxPosition, _boxSize, quaternion.identity, Jumpable);
+        if (isGrounded)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_boxPosition + Vector3.up * .5f, Vector3.down, out hit, 1, Jumpable))
+            {
+                hitNormal = hit.normal;
+                //Debug.Log(hit.transform.name);
+            }
+        }
+        else
+        {
+            hitNormal = hitNormal * .99f;
+        }
+        isSliding = (!(Vector3.Angle(Vector3.up, hitNormal) <= slopeLimit));
+    }
+
+    /// <summary> Making the player to slide on steep slopes. </summary>
+    private void slide()
+    {
+        if (isSliding)
+        {
+            Vector3 slid = Vector3.zero;
+            slid.x += ((1f - hitNormal.y) * hitNormal.x * SlideSpeed);
+            slid.z += ((1f - hitNormal.y) * hitNormal.z * SlideSpeed);
+
+            CC.Move(slid*Time.deltaTime);
+        }
+    }
+
+
+    /// <summary> Adding custom force to the character controller </summary>
+    public void AddForce(Vector3 dir, float force)
+    {
+        _forceDirection = dir;
+        _forceStrength  = force;
+    }
+
+    /// <summary> Makes the added force move the player Overtime. </summary>
+    private void applyingForce()
+    {
+        if (_forceStrength > 0)
+        {
+            CC.Move(_forceDirection.normalized * _forceStrength * Time.deltaTime);
+            _forceStrength -= _forceStrength * 2 * Time.deltaTime;
+        }
+    }
+
+    /// <summary> When the Player Spawns, This takes care of him. </summary>
+    private void setUpPlayer()
+    {
+        _inputHandler.horizontal = _playerInput.actions.FindAction("Look"); // Set up Looking Controls 
+        PlayerNumber = GM.AddPlayer(gameObject); // Give The Player a Number
+        switch (PlayerNumber) // Makes Cinemachine work with local Multiplayer
+        {
+            case 1: cfl.gameObject.layer = LayerMask.NameToLayer("Player1"); _cam.cullingMask = GM.PlayerMasks[0];
                 break;
             case 2:
-                cfl.gameObject.layer = LayerMask.NameToLayer("Player2"); MyCam.cullingMask = GM.PlayerMasks[1];
+                cfl.gameObject.layer = LayerMask.NameToLayer("Player2"); _cam.cullingMask = GM.PlayerMasks[1];
                 break;
             case 3:
-                cfl.gameObject.layer = LayerMask.NameToLayer("Player3"); MyCam.cullingMask = GM.PlayerMasks[2];
+                cfl.gameObject.layer = LayerMask.NameToLayer("Player3"); _cam.cullingMask = GM.PlayerMasks[2];
                 break;
             case 4:
-                cfl.gameObject.layer = LayerMask.NameToLayer("Player4"); MyCam.cullingMask = GM.PlayerMasks[3];
+                cfl.gameObject.layer = LayerMask.NameToLayer("Player4"); _cam.cullingMask = GM.PlayerMasks[3];
                 break;
             default: return;
         }
     }
 
+    /// <summary> Triggers when the player disconnects his controller </summary>
+    public void Leave()
+    {
+        if (GM.LeaveOnDisconnect) // Only Remove Player if LeaveOnDisconnect is True
+        {
+            GM.PlayerLeft(PlayerNumber); // Tell The Game Manager we Removed a Player
+            Destroy(gameObject); // Remove The Player
+        }
+    }
+
+    // Inputs 
     public void OnMove(InputAction.CallbackContext context)
     {
-        movement = context.ReadValue<Vector2>();
+        _movement = context.ReadValue<Vector2>();
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-       Jumped  = context.action.triggered;
-    }
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        look = context.ReadValue<Vector2>();
+       _jumped  = context.action.triggered;
     }
 
+    //Gizmos
     void OnDrawGizmosSelected()
     {
-        // Draw a line in the Editor to show whether we are touching the ground.
-        Gizmos.color = isGrounded ? Color.blue : Color.red; Gizmos.DrawCube(BoxOrigin, RayCastSize * 2);
-        //Debug.DrawLine(RaycastOrigin, RaycastOrigin + Vector3.down * RaycastDistance, isGrounded ? Color.white : Color.red);
+        // Draw a Box in the Editor to show whether we are touching the ground, Blue is Touching, Red is Not Touching.
+        Gizmos.color = isGrounded ? Color.blue : Color.red; Gizmos.DrawCube(_boxPosition, _boxSize * 2);
     }
 }
+
