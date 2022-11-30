a575add98de01ac7b1fa8bc47bc2d57e8913f40a
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2022-11-30 14:22:12 +0200

    READONLY

--- Assets/Our Stuff/Scripts/Data/Health.cs
+++ Assets/Our Stuff/Scripts/Data/Health.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using UnityEngine;
 public abstract class Health : MonoBehaviour
 {
     //data
     public float MaxHealth;
-    public float CurrentHealth;
-    public bool _isDead;
+    [ReadOnly][SerializeField] float CurrentHealth;
+    private bool _isDead;
+
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
 
     public abstract void TakeDamage(float damage, float knockback, Vector3 pushFrom);
 
