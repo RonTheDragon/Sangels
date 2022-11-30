f02de4561f986fc0f0f5b93a60cc36a8805797f4
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2022-11-30 13:53:45 +0200

    health player

--- Assets/Our Stuff/Scripts/Data/PlayerHealth.cs
+++ Assets/Our Stuff/Scripts/Data/PlayerHealth.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth
+public class PlayerHealth : Health
 {
-
+    public override void TakeDamage(float damage, float knockback, Vector3 pushFrom)
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void TakeFire()
+    {
+        throw new NotImplementedException();
+    }
+    public override void TakeStun()
+    {
+        throw new NotImplementedException();
+    }
 }
