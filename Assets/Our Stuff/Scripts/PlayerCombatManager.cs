04dea20ec945e3c2a4cafd9d9f5ed803db9fabfd
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2022-11-30 23:10:59 +0200

    fixing inheritence errors

--- Assets/Our Stuff/Scripts/PlayerCombatManager.cs
+++ Assets/Our Stuff/Scripts/PlayerCombatManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
-public class PlayerCombatManager : CombatS/ystem
+public class PlayerCombatManager : AttackManager
 {
     //Serializefield 
     [Tooltip("shooting cooldown")]
