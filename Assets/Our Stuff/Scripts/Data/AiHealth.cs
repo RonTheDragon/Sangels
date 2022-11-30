8f0787a7ee46ee4a920fcf403a9bef60a29b903b
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2022-11-30 13:52:24 +0200

    fixing

--- Assets/Our Stuff/Scripts/Data/AiHealth.cs
+++ Assets/Our Stuff/Scripts/Data/AiHealth.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class AiHealth : Health
@@ -12,5 +14,26 @@ public class AiHealth : Health
             gameObject.SetActive(false);
         }
     }
+
+
+    public override void TakeDamage(float damage, float knockback, Vector3 pushFrom)
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void TakeFire()
+    {
+        throw new NotImplementedException();
+    }
+    public override void TakeStun()
+    {
+        throw new NotImplementedException();
+    }
     
+
+
+
+
+
+
+
 }
