4ae9a04e7533a41f71b8d7eff2750b6ebe3c1877
RonTheDragon <gweta.ron@gmail.com>
2022-12-20 12:04:44 +0200

    better spawning

--- Assets/Our Stuff/Scripts/Game Managers/GameManager.cs
+++ Assets/Our Stuff/Scripts/Game Managers/GameManager.cs
@@ -32,7 +32,10 @@ public class GameManager : MonoBehaviour
 
     [HideInInspector] public static GameManager instance;
     bool _turnedOffStartingCam;
-    GameObject _noPlayersCamera => transform.GetChild(0).gameObject;
+
+    GameObject _playerSpawner => transform.GetChild(0).gameObject;
+    GameObject _noPlayersCamera => _playerSpawner.transform.GetChild(0).gameObject;
+    Transform _playerSpawnPoint => _playerSpawner.transform.GetChild(1);
 
     // Starts Before The Game Starts
     private void Awake()
@@ -65,6 +68,18 @@ public class GameManager : MonoBehaviour
     /// <summary> Players Call this Function When they join to update the Game Manager </summary>
     public int AddPlayer(GameObject Player)
     {
+        CharacterController CC = Player.GetComponent<CharacterController>();
+        CC.enabled = false;
+        if (0 == CountPlayers())
+        { 
+            Player.transform.position = _playerSpawnPoint.position;
+        }
+        else
+        {
+            Player.transform.position = GetLeadPlayer().transform.position + Vector3.up*2;
+        }
+        CC.enabled = true;
+
         for (int i = 0; i < 4; i++)
         {
             if (Players[i] == null)
@@ -72,8 +87,22 @@ public class GameManager : MonoBehaviour
                 Players[i] = Player;
                 return (i+1);
             }
-        } 
+        }
+
+
         return 0;
+    }
+
+    GameObject GetLeadPlayer()
+    {
+        foreach(GameObject p in Players)
+        {
+            if(p != null)
+            {
+                return p;
+            }
+        }
+        return null;
     }
 
     /// <summary> Returns The Amount of Players Currently Playing </summary>
@@ -114,6 +143,12 @@ public class GameManager : MonoBehaviour
             }
         }
         return coliders[ClosestColliderIndex];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(_playerSpawnPoint.position, Vector3.one);
     }
 }
 
